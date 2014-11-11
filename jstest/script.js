58cee4e5873d09ebdb9cadb00afc9904633d02b6
marre <saeztoro.mauricio@gmail.com>
2014-11-11 09:56:56 +0100

    Added comments for js script

--- jstest/script.js
+++ jstest/script.js
@@ -1,10 +1,9 @@
 
 $(document).ready(function(){
-    var haha = "men vad fan"
     var iCount = 0;
     var addDiv = "<div class='ruta1 divJS'><div class='inputBox'><input type=text class='input' id=tb value=''></div><br/><strong>MouseOverMIG!<div class='deleteS'>x</div></strong></div>";
 	
-
+// Creates max 11 div boxes and gives them a different ID number.
  	$(".button1").click(function(){
 		if (iCount <= 10) {
 		iCount = iCount + 1;
@@ -16,6 +15,7 @@ $(document).ready(function(){
 		}
 	});
 
+// Deletes a hole .divJS(box) with .deleteS
 	$(document).on("click",".deleteS",function(){
 	   $(this).parent().parent().remove();
 	});
