5d38c04d3915a6c05819f62bcf6713d919e18169
marre <saeztoro.mauricio@gmail.com>
2014-11-19 08:15:05 +0100

    Getting php to connect mysql to content in site

---
+++ jstest/index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+	<title>Testar ut mina jQuery skillz</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+      	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="stylesheetMarre.css"/>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>	
+	<script type="text/javascript" src="script.js"></script>
+
+    </head>
+   <body>
+      <div id="Big">
+		<div class="infoButton">Info</div>
+		   <div class="infoDropdown">I made this site just to save and get access to Url's from my smartphone that i get on various chat's. Enjoy!</div>
+
+
+	  <center><div class="addButton">Add</div></center><br>
+	  <center><div class="saveButton">Save</div></center>
+<!--	  <center><div class="button2">Remove</div></center>--!> 
+
+	<br><br>
+	<div class='ruta1 divJS'><div class='inputBox'><form><input type=text><input type="submit"></form></div><strong></strong></div>
+
+      </div>
+
+	<div id="footerBig2"><strong>by:</strong><p>marre</p></div>
+	<span></span>
+
+
+
+
+   </body>
+</html>
