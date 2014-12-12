ebea11a9098e4e9de3660deffbc3cdc4158b894b
marre <saeztoro.mauricio@gmail.com>
2014-12-12 09:59:23 +0100

    Added a form to each Box (divJS), and a own submit input of course

--- jstest/script.js
+++ jstest/script.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function(){
     var iCount = 0;
-    var addDiv = "<div class='ruta1 divJS'><div class='inputBox'><form><input type=text><input type='submit'></form></div><strong><div class='deleteS'>x</div></strong></div>";
+    var addDiv = "<form class='forM' method='POST'><div class='ruta1 divJS'><div class='deleteS'><strong>x</strong></div><div class='inputBox'><input class='inpuT' type='text'><input class='submitThis'  type='submit'></div><strong></strong><a href='#'><div class='adressBox'></div></a></div></form>";
+
 	
 	$(".infoButton").click(function(){
 	   $(".infoDropdown").slideToggle("slow");
 	});
-
 
 // Remove and add .buttonColor class to Add button
 
@@ -23,32 +23,37 @@ $(document).ready(function(){
 	   $(".saveButton").removeClass("buttonSavecolor");
 	});
 
-// Creates max 11 div boxes and gives them a different ID number.
- 	$(".addButton").click(function(){
+// Creates max 9 div boxes and gives them a different ID number.
+ 	$(document).on('click touchstart', '.addButton', function(){
 		if (iCount <= 7) {
 		iCount = iCount + 1;
 		console.log(iCount);
 		$("#Big").append($(addDiv));
         	   $(".divJS").each(function(i) {
                	   $(this).attr("id", (i + 1)); 
-                });
+                   });
+        	      $(".inpuT").each(function(i) {
+               	      $(this).attr("name", "url-" + (i + 1)); 
+                      });
+        	         $(".submitThis").each(function(i) {
+               	         $(this).attr("name", "submit" + (i + 1)); 
+                         });
 		}
 	});
+
 
 // Deletes a hole .divJS(box) with .deleteS and counts down for every click so that it follows the structure of the ADD button.
 	$(document).on("click",".deleteS",function(){
 	if (iCount => 7)
 	   iCount = iCount - 1;
 		console.log(iCount);
-	   $(this).parent().parent().remove();
+	   $(this).parent().remove();
 	});
 
-	$('.divJS').submit(function(event){
-	      $(this).text('TRUE').show();
-		console.log("TRUE");
-		return;
-	$('span').text('FALSE bro..').show();
-	console.log("FALSE");
+// Submitting every box (divJS) with saveButon
+	$('.saveButton').click(function(){
+		$("form").submit();
+	console.log("ClickClick..:");
 	});
 
 });
