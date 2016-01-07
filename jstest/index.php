52b739ec182fe325f442b3cc46e91c50ff0963d4
marre <saeztoro.mauricio@gmail.com>
2016-01-07 15:35:26 +0100

    Changed database to sqlite, Php is rendering the boxes from now on.., and there are some minor changes 2..

--- jstest/index.php
+++ jstest/index.php
@@ -1,11 +1,11 @@
 <html>
-    <head>
-	<title>Testar ut mina jQuery skillz</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>	
-	<script type="text/javascript" src="script.js"></script>
-	<link rel="stylesheet" type="text/css" href="stylesheetMarre.css"/>
+  <head>
+    <title>Testar ut mina jQuery skillz</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>	
+    <script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" type="text/css" href="stylesheetMarre.css"/>
 
 
 <style>
@@ -16,209 +16,117 @@
 
 
 
-    </head>
-   <body>
-<?php
-#var_dump ($_POST);
+  </head>
+<body>
+<div id="Big">
+  <div class="infoButton">Info</div>
+  <div class="infoDropdown">I made this site just to save and get access to Url's from my smartphone that i get on various chat's. Enjoy!</div>
 
-#foreach ($_POST as $k=>$v) {
-#  echo '<script>$(".inpuT").attr("value", "'.$v.'");</script>';
+<!--  <center><div class="addButton">Add</div></center><br>-->
+  <div class="text1">Submit the url you want to save</div>
+  <br>
+
+<?php
+
+echo "<form class=\"postThis\" method=\"POST\">
+        <input type=\"text\" name=\"onlyInput\">
+        <input class=\"OnlySubmit\" type=\"submit\" name=\"onlySubmit\" value=\"1\">
+      </form>
+     ";
+
+
+echo "<br><br>";
+$db = new SQLite3('jstestDB.sqlite');
+$result = $db->query("SELECT url_id, url FROM url");
+$i = 1;
+$f = 1;
+$url_id = "WHOHA";
+while ($row = $result->fetchArray(SQLITE3_NUM)) {
+        $url_id = $row[0];
+        $url    = $row[1];
+        $parse  = parse_url($url);
+        echo "<form class=\"forM\" method=\"POST\">
+                <div class=\"ruta1 divJS\">
+                  <div class=\"url_id\">${url_id}</div>
+                  <div class='deleteS'><strong>x</strong></div>
+                    <a href=\"${url}\">
+                    <div class=\"adressBox\"></div><!--.adressBox-->
+                    </a>
+                <div class=\"urlText\">${parse['host']}</div>
+                </div><!--.divJS-->
+              </form>
+             ";
+}
+
+date_default_timezone_set('Europe/Stockholm');
+#include('jstestThephp.php');
+
+$db = new SQLite3('jstestDB.sqlite');
+$urlID = ('');
+
+# New url:
+if (array_key_exists('onlySubmit', $_POST) && $_POST['onlySubmit'] == '1') {
+    $newUrl = $_POST['onlyInput'];
+    $db->exec("INSERT OR REPLACE INTO url (url) VALUES ('$newUrl')");
+    header("Location: http://${_SERVER['SERVER_NAME']}/jstest-master/jstest/index.php", true, 303);
+    exit();
+}
+ 
+# Delete:
+if (array_key_exists('action', $_POST)) {
+  if ($_POST['action'] == 'removeUrl') {
+    $db->exec("DELETE FROM url WHERE url_id = ${_POST['urlId']}");
+  }
+  header("Location: http://${_SERVER['SERVER_NAME']}/jstest-master/jstest/index.php", true, 303);
+  exit();
+
+}
+
+
+
+?>
+
+
+
+</div><!---#Big--!>
+
+<div id="footerBig2"><strong>by:</strong><p>marre</p></div>
+<br><br>
+
+
+<?php
+#-------------------------------1-------------------------------------
+#$urlpost = $db->query('SELECT * FROM url');
+#echo "<br><br><br>";
+#echo "<div style='border:1px solid black;'>";
+#while($tableName = $urlpost->fetchArray()) {
+#  echo "<pre style='width:;'>" ;echo json_encode($tableName,JSON_PRETTY_PRINT); echo "</pre>";
+#}   
+#echo "</div>";
+
+
+#echo "<h1>$url_id</h1>";
+
+#   $table = $tableName[0];
+#
+#   echo '<h3>',$table,'</h3>';
+#   $urlpost2 = mysql_query('SELECT * FROM test') or die('Cannot show tables from'.$table);
+#   if ( mysql_num_rows($urlpost2)) {
+#      echo '<table cellpadding="0" cellspacing="0" class="db-table table" style="color: #fffff;">';
+#      echo '<tr><th>ID</th><th>Url</th><th>nothing</th><tr>';
+#         while ($row2 = mysql_fetch_row($urlpost2)) {
+#            echo '<tr>';
+#            foreach ($row2 as $key=>$value) {
+#               echo '<td>',$value,'</td>';
+#            }
+#            echo '</tr>';
+#            }
+#            echo '</table><br>';
+#   }
 #}
 
 ?>
-      <div id="Big">
-		<div class="infoButton">Info</div>
-		   <div class="infoDropdown">I made this site just to save and get access to Url's from my smartphone that i get on various chat's. Enjoy!</div>
 
 
-	  <center><div class="addButton">Add</div></center><br>
-<!--	  <center><div class="saveButton">Save</div></center>--!>
-<!--	  <center><div class="button2">Remove</div></center>--!> 
-
-	<br><br>
-
-      <form class="forM" method="POST">
-	<div class='ruta1 divJS'>
-		<div class='inputBox'>
-		      <input class="inpuT" type="text" name="url-1" >
-		      <input class="submitThis" type="submit" name="submit1">
-		</div><!--inputBox--!>
-	<a href="<?php echo $_POST['url-1']; ?>">	
-	<div class="adressBox"></div><!--.adressBox--!>
-	</a>
-	</div><!--.divJS--!>
-      </form>
-<!------------.1------------!>
-
-
-
-      <form class="forM" method="POST">
-	<div class='ruta1 divJS'>
-		<div class='inputBox'>
-		      <input class="inpuT" type="text" name="url-2" >
-		      <input class="submitThis" type="submit" name="submit2">
-		</div><!--inputBox--!>
-	<a href="<?php echo $_POST['url-2']; ?>">	
-	<div class="adressBox"></div><!--.adressBox--!>
-	</a>
-	</div><!--.divJS--!>
-      </form>
-<!------------.2------------!>
-
-      <form class="forM" method="POST">
-	<div class='ruta1 divJS'>
-		<div class='inputBox'>
-		      <input class="inpuT" type="text" name="url-3" >
-		      <input class="submitThis" type="submit" name="submit3">
-		</div><!--inputBox--!>
-	<a href="<?php echo $_POST['url-3']; ?>">	
-	<div class="adressBox"></div><!--.adressBox--!>
-	</a>
-	</div><!--.divJS--!>
-      </form>
-<!------------.3------------!>
-
-      <form class="forM" method="POST">
-	<div class='ruta1 divJS'>
-		<div class='inputBox'>
-		      <input class="inpuT" type="text" name="url-4" >
-		      <input class="submitThis" type="submit" name="submit4">
-		</div><!--inputBox--!>
-	<a href="<?php echo $_POST['url-4']; ?>">	
-	<div class="adressBox"></div><!--.adressBox--!>
-	</a>
-	</div><!--.divJS--!>
-      </form>
-<!------------.4------------!>
-
-      <form class="forM" method="POST">
-	<div class='ruta1 divJS'>
-		<div class='inputBox'>
-		      <input class="inpuT" type="text" name="url-5" >
-		      <input class="submitThis" type="submit" name="submit5">
-		</div><!--inputBox--!>
-	<a href="<?php echo $_POST['url-5']; ?>">	
-	<div class="adressBox"></div><!--.adressBox--!>
-	</a>
-	</div><!--.divJS--!>
-      </form>
-<!------------.5------------!>
-
-      <form class="forM" method="POST">
-	<div class='ruta1 divJS'>
-		<div class='inputBox'>
-		      <input class="inpuT" type="text" name="url-6" >
-		      <input class="submitThis" type="submit" name="submit6">
-		</div><!--inputBox--!>
-	<a href="<?php echo $_POST['url-6']; ?>">	
-	<div class="adressBox"></div><!--.adressBox--!>
-	</a>
-	</div><!--.divJS--!>
-      </form>
-<!------------.6------------!>
-
-
-      </div><!---#Big--!>
-
-	<div id="footerBig2"><strong>by:</strong><p>marre</p></div>
-
-<?php
-date_default_timezone_set('Europe/Stockholm');
-include('../sqlpass.php');
-
-$urlONE   = $_POST['url-1'];
-$urlTWO   = $_POST['url-2'];
-$urlTHREE = $_POST['url-3'];
-$urlFOUR  = $_POST['url-4'];
-$urlFIVE  = $_POST['url-5'];
-$urlSIX   = $_POST['url-6'];
-$urlSEVEN = $_POST['url-7'];
-$urlEIGHT = $_POST['url-8'];
-$urlNINE  = $_POST['url-9'];
-
-//Create connection
-$conn = mysql_connect($servername, $username, $password);
-$selectedDB = mysql_select_db($dbname,$conn) or die ("Could not select $dbname");
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
-    echo "Connection failed";
-}
-echo "Connected successfully";
-echo "<br>";
-echo "<br>";
-
-$connbase = mysql_select_db($dbname, $conn);
-if (!$connbase) {
-   die ('Can\'t use myDB : ' . mysql_error());
-}
-echo "Connected with database!";
-
-if (!empty($_POST[submit1])){
-$sql="INSERT INTO test (id) VALUES(1) ON DUPLICATE KEY UPDATE name='$urlONE'";
-} elseif (!empty($_POST[submit2])){
-$sql="INSERT INTO test (id) VALUES(2) ON DUPLICATE KEY UPDATE name='$urlTWO'";
-} elseif (!empty($_POST[submit3])){
-$sql="INSERT INTO test (id) VALUES(3) ON DUPLICATE KEY UPDATE name='$urlTHREE'";
-} elseif (!empty($_POST[submit4])){
-$sql="INSERT INTO test (id) VALUES(4) ON DUPLICATE KEY UPDATE name='$urlFOUR'";
-} elseif (!empty($_POST[submit5])){
-$sql="INSERT INTO test (id) VALUES(5) ON DUPLICATE KEY UPDATE name='$urlFIVE'";
-} elseif (!empty($_POST[submit6])){
-$sql="INSERT INTO test (id) VALUES(6) ON DUPLICATE KEY UPDATE name='$urlSIX'";
-} elseif (!empty($_POST[submit7])){
-$sql="INSERT INTO test (id) VALUES(7) ON DUPLICATE KEY UPDATE name='$urlSEVEN'";
-} elseif (!empty($_POST[submit8])){
-$sql="INSERT INTO test (id) VALUES(8) ON DUPLICATE KEY UPDATE name='$urlEIGHT'";
-} elseif (!empty($_POST[submit9])){
-$sql="INSERT INTO test (id) VALUES(9) ON DUPLICATE KEY UPDATE name='$urlNINE'";
-} 
-
-$myData = mysql_query($sql,$conn);
-$myRS = mysql_fetch_array($myData);
-#var_dump($myRS);
-
-$sql = "SELECT * FROM test";
-$myData = mysql_query($sql,$conn);
-  while($record = mysql_fetch_array($myData)){
-  }
-
-
-#-------------------------------1-------------------------------------
-$urlpost = mysql_query('SHOW TABLES',$conn) or die('Cannot show tables');
-while($tableName = mysql_fetch_row($urlpost)) {
-   
-   $table = $tableName[0];
-
-   echo '<h3>',$table,'</h3>';
-   $urlpost2 = mysql_query('SELECT * FROM test') or die('Cannot show tables from'.$table);
-   if ( mysql_num_rows($urlpost2)) {
-      echo '<table cellpadding="0" cellspacing="0" class="db-table table" style="color: #fffff;">';
-      echo '<tr><th>ID</th><th>Url</th><th>nothing</th><tr>';
-         while ($row2 = mysql_fetch_row($urlpost2)) {
-            echo '<tr>';
-            foreach ($row2 as $key=>$value) {
-               echo '<td>',$value,'</td>';
-            }
-            echo '</tr>';
-            }
-            echo '</table><br>';
-   }
-}
-
-
-
-
-
-
-
-
-?>
-
-
-
-
-
-   </body>
+</body>
 </html>
--- jstest/script.js
+++ jstest/script.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
                	   $(this).attr("id", (i + 1)); 
                    });
         	      $(".inpuT").each(function(i) {
-               	      $(this).attr("name", "url-" + (i + 1)); 
+               	      $(this).attr("name", "url" + (i + 1)); 
                       });
         	         $(".submitThis").each(function(i) {
                	         $(this).attr("name", "submit" + (i + 1)); 
@@ -51,9 +51,14 @@ $(document).ready(function(){
 
 // Deletes a hole .divJS(box) with .deleteS and counts down for every click so that it follows the structure of the ADD button.
 	$(document).on("click",".deleteS",function(){
-	if (iCount => 7)
-	   iCount = iCount - 1;
-		console.log(iCount);
+        var url_id = $(this).prev().text()
+        if (!confirm('Are you sure you want to remove this shit?')){
+            return;
+        }
+        $.post('',{action:'removeUrl', urlId: url_id})
+//	if (iCount => 7)
+//	   iCount = iCount - 1;
+//		console.log(iCount);
 	   $(this).parent().remove();
 	});
 
